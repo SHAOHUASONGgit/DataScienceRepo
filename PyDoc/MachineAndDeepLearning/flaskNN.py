51bc09b53c1573987cea54c1912866894ca50c7d
SHAOHUASONGgit <89825814+SHAOHUASONGgit@users.noreply.github.com>
2021-11-25 18:03:59 +0800

    dockerFinaltest

--- PyDoc/MachineAndDeepLearning/flaskNN.py
+++ PyDoc/MachineAndDeepLearning/flaskNN.py
@@ -2,7 +2,7 @@ import torch
 import pandas as pd
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
+#docker run -it -p 5000:5000 pytorchlightning/pytorch_lightning:1.5.3-py3.6-torch1.6
 def getLabels(labels):
     allLabels = []
     for data in labels:
@@ -30,7 +30,6 @@ def result():
                 return render_template('input.html')
             else:
                 toClassify.append(float(data[1]))
-        net = torch.load("model.pkl")
         raw = list(net(torch.tensor(toClassify).float()))
         predction = classfy(raw, allLabels)
         result = str("这个品种应该是：" + predction)
