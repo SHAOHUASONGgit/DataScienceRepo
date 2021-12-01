c7cf26d27b25c30942deace04aec3ed2f05c5505
SHAO_HUASONG <108973423@qq.com>
2021-12-01 11:09:29 +0800

    update 图像增强

--- PyDoc/Hadoop/mapper.py
+++ PyDoc/Hadoop/mapper.py
@@ -2,11 +2,11 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     data = line.split('\t')
-    if len(data)==2:  # 如果用户有好友
+    if len(data)==2:
         user = data[0]
         friends = data[1].split(',')
         for firstPointer in range(len(friends)):
-            print(user + "," + friends[firstPointer] + "\t" + "-1")  # 已经是好友了
+            print(user + "," + friends[firstPointer] + "\t" + "-1")
             for secondpointer in range(len(friends)):
                 if firstPointer!=secondpointer:
-                    print(friends[firstPointer] + "," + friends[secondpointer] + "\t" + user)  # 输出所有可能的朋友组合
+                    print(friends[firstPointer] + "," + friends[secondpointer] + "\t" + user)
