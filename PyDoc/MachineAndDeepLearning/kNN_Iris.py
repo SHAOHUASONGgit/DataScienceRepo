82b3659ce560e9363f6212c4a051f40db0cf6e3c
SHAOHUASONGgit <89825814+SHAOHUASONGgit@users.noreply.github.com>
2021-12-09 05:05:46 +0800

    数据分析与统计推断期末相关

--- PyDoc/MachineAndDeepLearning/kNN_Iris.py
+++ PyDoc/MachineAndDeepLearning/kNN_Iris.py
@@ -8,11 +8,11 @@ def dataReader(fileName):
     return dataset
 
 
-def caculateDistance(startPoint, endPoint):
+def caculateDistance(startPoint, endPoint):  # 计算欧氏距离
     return sum((startPoint-endPoint)**2)**0.5
 
 
-def iriskNN_Classify(dataset, input, decideNumber, specialPoint):
+def iriskNN_Classify(dataset, input, decideNumber, specialPoint):  # 改进kNN分类
     length = len(dataset)
     distances = []
     labels = {}
@@ -45,7 +45,7 @@ def iriskNN_Classify(dataset, input, decideNumber, specialPoint):
         return specialPointPrediction
 
 
-def elder_iriskNN_Classify(dataset, input, decideNumber):
+def traditional_iriskNN_Classify(dataset, input, decideNumber):  # 传统kNN分类
     length = len(dataset)
     distances = []
     labels = {}
@@ -66,7 +66,7 @@ def elder_iriskNN_Classify(dataset, input, decideNumber):
     return kNNPrediction
 
 
-def elder_check(dataset, decideNumber, specialPoint):
+def elder_check(dataset, decideNumber):
     labels = {}
     falseLabel = {}
     result = {}
@@ -76,17 +76,17 @@ def elder_check(dataset, decideNumber, specialPoint):
             labels[realLabel] = 0
             falseLabel[realLabel] = 0
         labels[realLabel] += 1
-        predictLabel = elder_iriskNN_Classify(dataset, data[:-1], decideNumber)
+        predictLabel = traditional_iriskNN_Classify(dataset, data[:-1], decideNumber)
         if predictLabel!=realLabel:
             falseLabel[realLabel] += 1
     for label in labels:
         total = labels[label]
         loss = falseLabel[label]
-        result[label] = (total-loss)/total
+        result[label] = str(100 * (total-loss)/total)+"%"
     return result
 
 
-def check(dataset, decideNumber, specialPoint):
+def check(dataset, decideNumber, specialPoint):  # 检验
     labels = {}
     falseLabel = {}
     result = {}
@@ -102,11 +102,11 @@ def check(dataset, decideNumber, specialPoint):
     for label in labels:
         total = labels[label]
         loss = falseLabel[label]
-        result[label] = (total-loss)/total
+        result[label] = str(100 * (total-loss)/total)+"%"
     return result
 
 
-def iriskNN_specialPoint_detector(dataset, decideNumber, specialRange):
+def iriskNN_specialPoint_detector(dataset, decideNumber, specialRange):  # 特殊点探测
     length = len(dataset)
     distances = []
     specialPoint = []
@@ -135,14 +135,15 @@ def iriskNN_specialPoint_detector(dataset, decideNumber, specialRange):
         distances.clear()
     return specialPoint
 
+# [5.9, 3.2, 4.8, 1.8] is Iris-versicolor
 
 dataset = dataReader('iris.csv')
-specialPoint = iriskNN_specialPoint_detector(dataset, 7, 1)
-# [5.9, 3.2, 4.8, 1.8] is Iris-versicolor
-prediction = elder_iriskNN_Classify(dataset, [5.9, 3.2, 4.8, 1.8], 7)
+prediction = traditional_iriskNN_Classify(dataset, [5.9, 3.2, 4.8, 1.8], 7)
 print("It is " + prediction)
-checkResult = elder_check(dataset, 7, specialPoint)
+checkResult = elder_check(dataset, 7)
 print(checkResult)
+
+specialPoint = iriskNN_specialPoint_detector(dataset, 7, 1)
 prediction = iriskNN_Classify(dataset, [5.9, 3.2, 4.8, 1.8], 7, specialPoint)
 print("It is " + prediction)
 checkResult = check(dataset, 7, specialPoint)
@@ -163,7 +164,7 @@ for data in dataset:
     y = data[1]
     z = data[2]
     if(data[-1] == "Iris-virginica"):
-        ax.scatter(x, y, z,c='#000000')
+        ax.scatter(x, y, z, c='#000000')
     elif(data[-1] == "Iris-versicolor"):
         ax.scatter(x, y, z, c='#ff0000')
     else:
