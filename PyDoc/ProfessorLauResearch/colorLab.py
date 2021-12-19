2081167e445a051b62f139dacb27f66a8320590a
SHAOHUASONGgit <89825814+SHAOHUASONGgit@users.noreply.github.com>
2021-12-19 23:06:14 +0800

    Update colorLab.py

--- PyDoc/ProfessorLauResearch/colorLab.py
+++ PyDoc/ProfessorLauResearch/colorLab.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage import exposure
+
 
 def gamma2Lab(inputMat):
     return np.where(inputMat > 0.0405, ((inputMat + 0.055) / 1.055) ** 2.4, inputMat / 12.92)
@@ -16,7 +15,7 @@ def gamma2BGR(inputMat):
 
 
 def funt2BGR(inputMat):
-    return np.where((inputMat ** 3) > 0.008856, inputMat ** 3, (inputMat -16 / 116) / 7.787)
+    return np.where((inputMat ** 3) > 0.008856, inputMat ** 3, (inputMat - 16 / 116) / 7.787)
 
 
 def bgr2BRG(inputMat):
@@ -26,9 +25,9 @@ def bgr2BRG(inputMat):
 
 def BGR2Lab(image):
     B, G, R = cv2.split(image.astype(np.float32))
-    B = gamma2Lab(B/255)
-    G = gamma2Lab(G/255)
-    R = gamma2Lab(R/255)
+    B = gamma2Lab(B / 255)
+    G = gamma2Lab(G / 255)
+    R = gamma2Lab(R / 255)
     X = funt2Lab((R * 0.4124 + G * 0.3576 + B * 0.1805) / 0.95047)
     Y = funt2Lab((R * 0.2126 + G * 0.7152 + B * 0.0722) / 1.00000)
     Z = funt2Lab((R * 0.0193 + G * 0.1192 + B * 0.9505) / 1.08883)
@@ -51,8 +50,28 @@ def Lab2BGR(image):
     B = bgr2BRG(gamma2BGR(X * 0.0557 + Y * -0.2040 + Z * 1.0570)).astype(np.uint8)
     return cv2.merge([B, G, R])
 
-imageB = cv2.imread("input/test.png")
-imageB2L = BGR2Lab(imageB)
-imageB2L2B = Lab2BGR(imageB2L)
 
-L, a, b = cv2.split(imageB2L)
+def histLab(inputMat, rangeSize, min):  # 有BUG！
+    hist = [0] * rangeSize
+    U = 0
+    totalPixels = inputMat.size
+    for pixel in inputMat.flat:
+        hist[pixel - min - 1] += 1 / totalPixels
+    for data in range(len(hist)):
+        U += (data + min) * hist[data]
+    return hist, U
+
+
+def BGR2LabColorReshape(image):
+    image = BGR2Lab(image)
+    L, a, b = cv2.split(image)
+    ahist, aU = histLab(a.astype(int), 255, -128)
+    bhist, bU = histLab(b.astype(int), 255, -128)
+    a = (a - aU).astype(np.float32)
+    b = (b - bU).astype(np.float32)
+    image = cv2.merge([L, a, b])
+    image = Lab2BGR(image)
+    return image
+
+imageB = cv2.imread("input/1.png")
+BGR2LabColorReshape(imageB)
