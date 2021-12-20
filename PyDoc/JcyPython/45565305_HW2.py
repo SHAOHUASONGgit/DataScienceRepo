d0eeda00c4d16408b578bae03e235007d1ffccfd
SHAOHUASONGgit <89825814+SHAOHUASONGgit@users.noreply.github.com>
2021-12-20 23:30:15 +0800

    jcypython

--- PyDoc/JcyPython/45565305_HW2.py
+++ PyDoc/JcyPython/45565305_HW2.py
@@ -10,8 +10,14 @@ for i in range(1,19):
     Houses = soup.find_all("div",class_="resblock-desc-wrapper")
     for item in Houses:
         name = item.find("a",class_="name").text
-        averageprice = item.find("span", class_="number").text + "元/m2"
-        area = item.find("div", class_="resblock-area").find("span").text.replace("建面 ", "").replace("㎡", "m2")
+        averageprice = item.find("span", class_="number").text
+        if averageprice == "":
+            averageprice = str(0)
+        averageprice += "元/m2"
+        area = item.find("div", class_="resblock-area").find("span").text.replace("建面 ", "").replace("㎡", "")
+        if area == "":
+            area = str(0)
+        area += "m2"
         region = item.find("div", class_="resblock-location").find("span").text
         output.write(name+", "+region+", "+averageprice+", "+area+"\n")
     time.sleep(10)#休眠以免封ip
